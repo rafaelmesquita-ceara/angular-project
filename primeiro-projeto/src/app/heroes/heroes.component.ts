a65ded903fc14bdda456593235ae8df25121d62f
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-08-08 23:10:07 -0300

    HeroService adicionado

--- primeiro-projeto/src/app/heroes/heroes.component.ts
+++ primeiro-projeto/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,12 +14,19 @@ export class HeroesComponent implements OnInit {
     id : 1,
     name : 'Windstorm'
   }
-  heroes = HEROES;
 
-  constructor() {
+  heroes : Hero[];
+
+  constructor(private heroService: HeroService) {
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   selectedHero : Hero;
